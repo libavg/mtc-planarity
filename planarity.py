20941575a22228851ab6e35c17b3a785d140a26e
scotty007 <scotty@c-base.org>
2009-12-31 09:20:03 +0000

    planarity: removed unneeded avg.button import
    svn path=/trunk/avg_media/mtc/planarity/; revision=4445

--- planarity.py
+++ planarity.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with planarity.  If not, see <http://www.gnu.org/licenses/>.
 
-from libavg import avg, button, Point2D, AVGApp
+from libavg import avg, Point2D, AVGApp
 from libavg.AVGAppUtil import getMediaDir
 
 from os import getenv
