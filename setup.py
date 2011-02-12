e8c6c56cd3fd2ad7ab70979a6614fd24d9440eaf
scotty007 <scotty@c-base.org>
2011-02-12 18:42:49 +0000

    avg_media/mtc/planarity: added Ka-Ping Yee to authors, added libavg url
    svn path=/trunk/avg_media/mtc/planarity/; revision=5549

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ from distutils.core import setup
 setup(
     name='Planarity',
     version='1.0',
-    author='Martin Heistermann, Thomas Schott',
-    author_email='<mh at sponc dot de>, <scotty at c-base dot org>',
+    author='Martin Heistermann, Thomas Schott, Ka-Ping Yee',
+    author_email='<mh at sponc dot de>, <scotty at c-base dot org>, <ping at zesty dot ca>',
+    url='https://www.libavg.de/',
     license='GPL3',
     packages=['planarity'],
     scripts=['scripts/planarity'],
